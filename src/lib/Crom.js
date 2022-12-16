afc813287cd91baca43808662fbfe08e7f9408b2
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2022-12-16 18:24:10 +0800

    update author page priority

--- src/lib/Crom.js
+++ src/lib/Crom.js
@@ -133,11 +133,11 @@ module.exports.init = ({discord}) => {
         let query = [...msg.content.matchAll(/\&(\[(?<branch>[a-zA-Z]{2,3})\])?(?<queri>.+)\&/gi)];
         let reply = [];
         for (var i = 0; i < query.length; i++) {
-          let {queri, branch} = query[i].groups
-          branch = branch ? branch.toLowerCase() : undefined
+          let {queri, branch} = query[i].groups;
+          branch = branch?.toLowerCase() ?? config.SCP_SITE;
           let filter = {
-            anyBaseUrl: !!branch&&!!branchUrls[branch] ? branchUrls[branch] : branchUrls[config.SCP_SITE],
-            baseUrl: !!branch&&!!branchUrls[branch] ? branchUrls[branch] : branchUrls[config.SCP_SITE]
+            anyBaseUrl: branchUrls[branch] ?? branchUrls[config.SCP_SITE],
+            baseUrl: branchUrls[branch] ?? branchUrls[config.SCP_SITE],
           }
           if (branch&&branch==="all") { filter.anyBaseUrl=null; filter.baseUrl=null; }
           let res = /^\#\d+$/.test(queri) ?
@@ -146,10 +146,15 @@ module.exports.init = ({discord}) => {
           res = res.data[/^\#\d+$/.test(queri) ? 'usersByRank' : 'searchUsers'];
           if (res.length) {
             let ans = res[0].name;
-            ans += `: ${!!branch&&(branch==="all"||!!branchUrls[branch]) ? branch.toUpperCase() : config.SCP_SITE.toUpperCase()} #${res[0].statistics.rank}`
-            ans += `\n共 ${res[0].statistics.pageCount} 頁面，總評分 ${res[0].statistics.totalRating}，平均分 ${res[0].statistics.meanRating}`
-            ans += res[0].authorInfos.length ? `\n作者頁：${res[0].authorInfos[0].authorPage.url}` : ""
-            reply.push(ans)
+            let authorInfo = res[0].authorInfos?.filter(v=>v.authorPage.url.startsWith(branchUrls[branch]));
+            ans += `: ${!!branch&&(branch==="all"||!!branchUrls[branch]) ? branch.toUpperCase() : config.SCP_SITE.toUpperCase()} #${res[0].statistics.rank}`;
+            ans += `\n共 ${res[0].statistics.pageCount} 頁面，總評分 ${res[0].statistics.totalRating}，平均分 ${res[0].statistics.meanRating}`;
+            if (authorInfo.length) {
+              ans += `\n作者頁：${authorInfo[0].authorPage.url}`;
+            } else if (res[0].authorInfos.length) {
+              ans += `\n作者頁：${res[0].authorInfos[0].authorPage.url}`;
+            }
+            reply.push(ans);
           }
         }
         if (reply.length) {
